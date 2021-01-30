f14bb1fc428b503f093d7acabd6753ac1ee4b2cb
zy_yutao@126.com <zy_yutao@126.com>
2021-01-30 23:25:20 +0800

    1

--- libs/layout/feature/ivms/src/lib/layout-feature-ivms.module.ts
+++ libs/layout/feature/ivms/src/lib/layout-feature-ivms.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainModule } from './main/main.module';
 import { SharedDataAccessStoreLvmsRootModule } from '@zyweb/shared/data-access/store/lvms-root';
+import { SharedDataAccessStoreNgrxRouterModule } from '@zyweb/shared/data-access/store/ngrx-router';
 
 @NgModule({
   imports: [CommonModule,
-    SharedDataAccessStoreLvmsRootModule.forRoot()
+    SharedDataAccessStoreLvmsRootModule.forRoot(),
+    SharedDataAccessStoreNgrxRouterModule,
   ],
   exports: [MainModule],
 })
