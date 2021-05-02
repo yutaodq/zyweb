6e5b45610c2fee23f9de37b9c85eff1bd47ce68b
zy_yutao@126.com <zy_yutao@126.com>
2021-05-02 12:25:06 +0800

    1

--- libs/shared/util/environments/src/lib/environment.prod.ts
+++ libs/shared/util/environments/src/lib/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
+  // baseUrl: 'http://localhost:9088/ivms',
   baseUrl: 'http://11.11.126.69:9088/ivms',
-  // clientApiUrl: 'http://11.11.126.69:9088/ivms',
 };
