ff0392c3a6929091b0a81678f3a7e73eb048de0e
zy_yutao@126.com <zy_yutao@126.com>
2021-05-08 09:57:19 +0800

    vehicle/use-state/main/src/lib/components/create-form/create-form.component.ts

--- libs/vehicle/use-state/main/src/lib/components/create-form/create-form.component.ts
+++ libs/vehicle/use-state/main/src/lib/components/create-form/create-form.component.ts
@@ -8,12 +8,13 @@ import {
   OnInit,
   Output
 } from '@angular/core';
-
-import { CreateFormPresenter } from './create-form.presenter';
+import { v4 as uuidv4 } from 'uuid';
 import { FormControl, FormGroup, ValidationErrors } from '@angular/forms';
 import { FormlyFieldConfig, FormlyFormBuilder, FormlyFormOptions } from '@ngx-formly/core';
-import { VehicleUseState } from '@zyweb/shared/data-access/model/lvms';
+
 import { Observable, of, Subscription } from 'rxjs';
+
+import { VehicleUseState } from '@zyweb/shared/data-access/model/lvms';
 // import { VehicleUseStateDataService } from '@zyweb/shared/data-access/api/lvms';
 import { MasterCreateCommand } from '@zyweb/shared/util/utility';
 import { CreateVehicleUseStateService } from '../../services';
@@ -22,8 +23,7 @@ import { CreateVehicleUseStateService } from '../../services';
   selector: 'zyweb-vehicle-use-state-create-form',
   templateUrl: './create-form.component.html',
   styleUrls: ['./create-form.component.scss'],
-  // providers: [CreateFormPresenter, VehicleUseStateDataService],
-  providers: [CreateFormPresenter],
+  providers: [],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
@@ -34,6 +34,10 @@ export class CreateFormComponent implements OnInit, OnDestroy {
 
   private subscriptions: Array<Subscription> = [];
 
+  private _form = new FormGroup({});
+
+  private _model: any = {};
+  private _options: FormlyFormOptions = {};
 
   public fields: FormlyFieldConfig[] =
     [
@@ -72,10 +76,10 @@ export class CreateFormComponent implements OnInit, OnDestroy {
     }
     ];
 
-  constructor(private _formPresenter: CreateFormPresenter,
-              private _createVehicleUseStateService: CreateVehicleUseStateService
-  ) {
-  }
+  // constructor(private _formPresenter: CreateFormPresenter,
+  //             private _createVehicleUseStateService: CreateVehicleUseStateService
+  // ) {  }
+  constructor(private _createVehicleUseStateService: CreateVehicleUseStateService) {  }
 
   ngOnInit(): void {
     this.registerEvents();
@@ -84,46 +88,87 @@ export class CreateFormComponent implements OnInit, OnDestroy {
   private registerEvents(): void {
     // 订阅车辆详情
     this.subscriptions.push(
-      this._formPresenter.add$.subscribe(vehicleUseState => this._command.onAdd(vehicleUseState)),
-      this._formPresenter.cancel$.subscribe(_ => this._command.onCancel()),
+      // this._formPresenter.add$.subscribe(vehicleUseState => this._command.onAdd(vehicleUseState)),
+      // this._formPresenter.cancel$.subscribe(_ => this._command.onCancel()),
     );
   }
 
   ngOnDestroy() {
     this.subscriptions.forEach(sub => sub.unsubscribe());
   }
-
+/*
+按钮命令
+ */
   onSubmit(model: any) {
-    this._formPresenter.save();
+    this.save();
+  }
+
+  public save(): void {
+    if (!this.isFormValid()) {
+      return;
+    }
+    const vehicleUseState: VehicleUseState = this.model as VehicleUseState;
+    /*
+      可写成 ( this.isEmpty(vehicle.id) ) && (vehicle.id = uuidv4());
+      但 tslint.json出现报警信息
+     */
+    if (this.isEmpty(vehicleUseState.id)) {
+      vehicleUseState.id = uuidv4();
+    }
+
+    Object.keys(vehicleUseState).forEach(
+      (key) => (vehicleUseState[key] === null || vehicleUseState[key] === '') && delete vehicleUseState[key]);
+
+    this.command.onAdd(vehicleUseState)
+  }
+
+  private isEmpty(id: string) {
+    let isEmpty = false;
+    if (id === undefined || id === null || id === '') {
+      isEmpty = true;
+    }
+    return isEmpty;
   }
 
   public cancelCreate(): void {
-    this._formPresenter.cancel();
+    this.command.onCancel();
   }
 
   public reset(): void {
-    this._formPresenter.reset();
+    this.options.resetModel();
   }
 
-  public get isFormValid() {
-    return this._formPresenter.isFormValid;
+  /*
+
+   */
+  public isFormValid() {
+    return this._form?.valid;
   }
 
-  canSave() {
+  public canSave() {
     return this.form.valid && this.form.pristine;
   }
 
-  get form(): FormGroup {
 
-    return this._formPresenter.form;
+
+  /*
+Get方法
+ */
+  get form(): FormGroup {
+    return this._form;
   }
 
   get model() {
-    return this._formPresenter.model;
+    return this._model;
   }
 
   get options(): FormlyFormOptions {
-    return this._formPresenter.options;
+    return this._options;
   }
+
+  get command(): MasterCreateCommand<VehicleUseState> {
+    return this._command;
+  }
+
 }
 
--- libs/vehicle/use-state/main/src/lib/components/create-form/create-form.presenter.ts
+++ libs/vehicle/use-state/main/src/lib/components/create-form/create-form.presenter.ts
@@ -1,78 +1,78 @@
-import { Injectable } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { Observable, of, Subject, timer } from 'rxjs';
-import { FormlyFormOptions } from '@ngx-formly/core';
-import { v4 as uuidv4 } from 'uuid';
-import { VehicleUseState } from '@zyweb/shared/data-access/model/lvms';
-
-
-@Injectable()
-export class CreateFormPresenter {
-  private _add: Subject<VehicleUseState> = new Subject();
-  add$: Observable<VehicleUseState> = this._add.asObservable();
-
-  private _cancel: Subject<string> = new Subject();
-  cancel$: Observable<string> = this._cancel.asObservable();
-
-  private _reset: Subject<string> = new Subject();
-  reset$: Observable<string> = this._reset.asObservable();
-
-  private _form = new FormGroup({});
-
-  private _model: any = {};
-  private _options: FormlyFormOptions = {};
-
-  public cancel(): void {
-    this._cancel.next('cancelCreate');
-  }
-
-  public reset(): void {
-    this.options.resetModel();
-  }
-
-  public isFormValid() {
-    return this._form?.valid;
-  }
-  canSave() {
-    return this.form.valid && this.form.pristine;
-  }
-
-  public save(): void {
-    if (!this.isFormValid()) {
-      return;
-    }
-    const vehicleUseState: VehicleUseState = this.model as VehicleUseState;
-    /*
-      可写成 ( this.isEmpty(vehicle.id) ) && (vehicle.id = uuidv4());
-      但 tslint.json出现报警信息
-     */
-    if (this.isEmpty(vehicleUseState.id)) {
-      vehicleUseState.id = uuidv4();
-    }
-
-    Object.keys(vehicleUseState).forEach(
-      (key) => (vehicleUseState[key] === null || vehicleUseState[key] === '') && delete vehicleUseState[key]);
-
-    this._add.next(vehicleUseState);
-  }
-
-  private isEmpty(id: string) {
-    let isEmpty = false;
-    if (id === undefined || id === null || id === '') {
-      isEmpty = true;
-    }
-    return isEmpty;
-  }
-
-  get form(): FormGroup {
-    return this._form;
-  }
-
-  get model() {
-    return this._model;
-  }
-
-  get options(): FormlyFormOptions {
-    return this._options;
-  }
-}
+// import { Injectable } from '@angular/core';
+// import { FormGroup } from '@angular/forms';
+// import { Observable, of, Subject, timer } from 'rxjs';
+// import { FormlyFormOptions } from '@ngx-formly/core';
+// import { v4 as uuidv4 } from 'uuid';
+// import { VehicleUseState } from '@zyweb/shared/data-access/model/lvms';
+//
+//
+// @Injectable()
+// export class CreateFormPresenter {
+//   private _add: Subject<VehicleUseState> = new Subject();
+//   add$: Observable<VehicleUseState> = this._add.asObservable();
+//
+//   private _cancel: Subject<string> = new Subject();
+//   cancel$: Observable<string> = this._cancel.asObservable();
+//
+//   private _reset: Subject<string> = new Subject();
+//   reset$: Observable<string> = this._reset.asObservable();
+//
+//   // private _form = new FormGroup({});
+//   //
+//   // private _model: any = {};
+//   // private _options: FormlyFormOptions = {};
+//
+//   public cancel(): void {
+//     this._cancel.next('cancelCreate');
+//   }
+//
+//   // public reset(): void {
+//   //   this.options.resetModel();
+//   // }
+//   //
+//   // public isFormValid() {
+//   //   return this._form?.valid;
+//   // }
+//   // canSave() {
+//   //   return this.form.valid && this.form.pristine;
+//   // }
+//
+//   // public save(): void {
+//   //   if (!this.isFormValid()) {
+//   //     return;
+//   //   }
+//   //   const vehicleUseState: VehicleUseState = this.model as VehicleUseState;
+//   //   /*
+//   //     可写成 ( this.isEmpty(vehicle.id) ) && (vehicle.id = uuidv4());
+//   //     但 tslint.json出现报警信息
+//   //    */
+//   //   if (this.isEmpty(vehicleUseState.id)) {
+//   //     vehicleUseState.id = uuidv4();
+//   //   }
+//   //
+//   //   Object.keys(vehicleUseState).forEach(
+//   //     (key) => (vehicleUseState[key] === null || vehicleUseState[key] === '') && delete vehicleUseState[key]);
+//   //
+//   //   this._add.next(vehicleUseState);
+//   // }
+//   //
+//   // private isEmpty(id: string) {
+//   //   let isEmpty = false;
+//   //   if (id === undefined || id === null || id === '') {
+//   //     isEmpty = true;
+//   //   }
+//   //   return isEmpty;
+//   // }
+//
+//   // get form(): FormGroup {
+//   //   return this._form;
+//   // }
+//   //
+//   // get model() {
+//   //   return this._model;
+//   // }
+//   //
+//   // get options(): FormlyFormOptions {
+//   //   return this._options;
+//   // }
+// }
